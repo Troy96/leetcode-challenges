0589088f114a2a138810fc60f14146a123f67f4b
Tuhin Roy <troy0870@gmail.com>
2020-06-02 07:42:25 +0530

    add: june-challenge: invert_binary-tree

---
+++ easy/june-challenge/invert_binary_tree.cpp
@@ -0,0 +1,52 @@
+// Invert a binary tree.
+
+// Example:
+
+// Input:
+
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
+// Output:
+
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
+
+#include <iostream>
+
+//Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == NULL)
+            return NULL;
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        TreeNode *temp;
+
+        temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        return root;
+    }
+};
